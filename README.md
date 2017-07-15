d6f43a8dee4399e2c7bfd6a49be2be4d7a6401f0
Vladimir Novick <vlad.novick@gmail.com>
2017-07-15 11:18:40 -0700

    add comments

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DesktopWallpaperChanger-Scheduler-Task
 Change Your Wallpaper Automatically by Time / Scheduler Task
+
+DesktopWallpaperChanger runs on 32-bit and 64-bit Microsoft Windows operating systems without installation.
+
+Configuration: SheduleDecktopChanger.exe.config
---
+++ Source/DesktopWallpaperChangerSchedulerTask/WallpaperServiceInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SheduleDecktopChanger
+{
+    [RunInstaller(true)]
+    public partial class WallpaperServiceInstaller : System.Configuration.Install.Installer
+    {
+        public WallpaperServiceInstaller()
+        {
+            InitializeComponent();
+        }
+    }
+}
